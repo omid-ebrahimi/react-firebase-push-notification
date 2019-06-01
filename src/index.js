7008cfca69c07d427123d8d8a942206973cdc3d7
omid-ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-01 12:07:12 +0430

    handles firebase notifications in foreground

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { initializeFirebase } from './push-notification';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 initializeFirebase();
+
+const messaging = firebase.messaging();
+messaging.onMessage(notification => {
+    alert('Notification: ' + JSON.stringify(notification))
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
